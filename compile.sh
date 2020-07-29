44a6b5631beb18af382835f2226c70494ab102f9
Forest Kobayashi <fkobayashi@hmc.edu>
2020-07-29 08:00:38 -0800

    update compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Compile the 181 paper
-cd report && cp proposal m181-paper && bmbcompile m181-paper && cd ..
+cd report && cp proposal.tex m181-paper.tex && bmbcompile m181-paper && cd ..
